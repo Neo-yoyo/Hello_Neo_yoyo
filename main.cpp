9b102203bb13342c23d8e164e658c9bf4413213b
Neo-yoyo <bluecloud9508@gmail.com>
2023-11-10 13:16:22 +1000

    edit subtree & main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+#include "subtree/vector.h"
+
 int main() {
-    std::cout << "Hello world!" << std::endl ;
+    /// Создаем вектор типа int и задаем его размер
+    Vector<int> v(5);
+
+    /// Вставляем элементы в вектор
+    v.PushBack(1);
+    v.PushBack(2);
+    v.PushBack(3);
+    v.PushBack(4);
+    v.PushBack(5);
+
+    /// Выводим элементы вектора
+    for (const auto& elem : v) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+
 }
 
