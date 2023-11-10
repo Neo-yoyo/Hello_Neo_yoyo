a513d530c5b39c66067d149ca84ba6cb158f8caf
Neo-yoyo <bluecloud9508@gmail.com>
2023-11-10 12:42:58 +1000

    удалила остатки подмодуля

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
-#include "git_submodul/submodule/calculator/calculator.hpp"
-
 int main() {
     std::cout << "Hello world!" << std::endl ;
-
-    calculator calc;
-
-     calc.add(7, 5);
 }
 
-/// Изменения
