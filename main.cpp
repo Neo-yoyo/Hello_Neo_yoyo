2c3aec045ca413041f060c7ee8ac7b0e9af55297
Neo-yoyo <bluecloud9508@gmail.com>
2023-11-10 12:22:07 +1000

    main update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
-#include "git_submodul/submodule/calculator/calculator.hpp"
+#include "git_submodule/submodule/calculator/calculator.hpp"
 
 int main() {
     std::cout << "Hello world!" << std::endl ;
 
     calculator calc;
 
-     calc.add(7, 5);
+    std::cout << calc.add(7, 5) << std::endl ;
 }
 
-/// Изменения
